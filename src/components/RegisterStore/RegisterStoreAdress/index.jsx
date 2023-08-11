6959945e1543be2ed4ecbeb731b791735bae9ce2
SooY2 <ssy67366736@gmail.com>
2023-08-11 09:13:42 +0900

    상세주소찾기

--- src/components/RegisterStore/RegisterStoreAdress/index.jsx
+++ src/components/RegisterStore/RegisterStoreAdress/index.jsx
@@ -1,10 +1,34 @@
 import styles from "./index.module.css";
+import DaumPostcode from "react-daum-postcode";
 
 const RegisterStoreAddress=()=>{
+
+    //상세주소검색
+    const handleComplete = (data) => {
+        let fullAddress = data.address;
+        let extraAddress = "";
+    
+        if (data.addressType === "R") {
+          if (data.bname !== "") {
+            extraAddress += data.bname;
+          }
+          if (data.buildingName !== "") {
+            extraAddress +=
+              extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+          }
+          fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+        }
+    
+        console.log(fullAddress);
+      };
+    
+    
+
     return (<div className={styles.wrapper}>
         <div>가게주소 등록하기</div>
         <button>
             <div></div>
+            {false&&<DaumPostcode/>}
             
         </button>
         <input className={styles.input} 
